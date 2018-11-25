f8f260e44884f61782636a572c33bd122064fe62
diffamr <44928835+diffamr@users.noreply.github.com>
2018-11-25 07:45:44 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBin_cr62CpLTLcpOxo45sfqzEk4KmhpvAhYKehLC724uKgMy7rYGwwM6Cla2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "49dd3c2eb8d1dee9";
+$os="android&version=2.1.6&access=billboard";
+$sign="121213213123";
